0d58a334400aa06ce1f1e1692c77263f2d5a5167
KevinBermudezC <kevinbermudezc@gmail.com>
2024-10-28 09:24:06 -0500

    :sparkles: Home page can fetch data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,12 @@
 import SearchForm from "@/components/searchForm/SearchForm";
-import StartupCard from "@/components/Card/StartupCard";
+import StartupCard, {StartupTypeCard} from "@/components/Card/StartupCard";
+import {STARTUPS_QUERY} from "@/sanity/lib/queries";
+import {client} from "@/sanity/lib/client";
 
 export default async function Home({searchParams}: {searchParams: Promise<{ query?: string}>}) {
   const query = (await searchParams).query;
-  const posts = [{
-    _createdAt: new Date(),
-    views: 55,
-    author: {_id:1, name: 'John Doe'},
-    _id:1,
-    description: 'A new way to connect with people',
-    image: 'https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    category: 'Tech',
-    title: 'Flying Robot'
 
-  }]
+  const posts = await client.fetch(STARTUPS_QUERY)
 
   return (
     <>
@@ -31,7 +24,7 @@ export default async function Home({searchParams}: {searchParams: Promise<{ quer
         </p>
 
         <ul className="mt-7 card_grid">
-          {posts.map((post : StartupCardType, index:number)=>(
+          {posts.map((post : StartupTypeCard)=>(
             <StartupCard key={post?._id} post={post} />
           ))}
         </ul>
--- components/Card/StartupCard.tsx
+++ components/Card/StartupCard.tsx
@@ -4,9 +4,11 @@ import {EyeIcon} from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import {Author, Startup} from "@/sanity/types";
 
+export type StartupTypeCard = Omit<Startup, "author"> & {author?: Author};
 const StartupCard = ({post}: {post: StartupTypeCard}) => {
-  const {_createdAt, author:{_id: authorId, name}, views, title, description, image, category, _id} = post;
+  const {_createdAt, author, views, title, description, image, category, _id} = post;
 
   return (
     <li className="startup-card group">
@@ -21,9 +23,9 @@ const StartupCard = ({post}: {post: StartupTypeCard}) => {
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
+          <Link href={`/user/${author?._id}`}>
             <p className="text-16-medium line-clamp-1">
-              {name}
+              {author?.name}
             </p>
           </Link>
           <Link href={`/startup/${_id}`}>
@@ -32,7 +34,7 @@ const StartupCard = ({post}: {post: StartupTypeCard}) => {
             </h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image src="https://placehold.co/48x48" alt='placeholder' width={48} height={48} className="rounded-full"/>
         </Link>
       </div>
@@ -41,7 +43,7 @@ const StartupCard = ({post}: {post: StartupTypeCard}) => {
         <img src={image} alt="image" className="startup-card_image"/>
       </Link>
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}`}>
+        <Link href={`/?query=${category?.toLowerCase()}`}>
           <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup-card_btn" asChild>
